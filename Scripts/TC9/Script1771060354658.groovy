95c3825c57d73101222fec17a8a8bc94eb30d99e
kazurayam <kazuaki.urayama@gmail.com>
2026-02-14 18:22:55 +0900

    #4

---
+++ Scripts/TC9/Script1771060354658.groovy
@@ -0,0 +1,17 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+/*
+ * demonstrate that we can retrieve the updated config file
+ */
+
+File f = new File('./myconfig2.json')
+println "[TC9] " + f.text
+
+JsonSlurper slurper = new JsonSlurper()
+def config = slurper.parse(f)
+
+println "[TC9] bill: " + config["bill"]
+println "[TC9] home: " + config["home"]
+println "[TC9] money: " + config["money"]
---
+++ Test Suites/TS2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fd74270e-b02c-41a5-9817-8913aeb4e509</testSuiteGuid>
+   <testCaseLink>
+      <guid>a56ee475-7326-4a0c-8170-5c0fe841b7bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC9</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
